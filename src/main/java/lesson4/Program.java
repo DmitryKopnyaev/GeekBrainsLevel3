6b335f8b3d3875d1159bdca5e35b66490113e03e
Dimentor <dk2491@mail.ru>
2021-03-27 10:49:44 +0400

    Lesson 4: homework begin

---
+++ src/main/java/lesson4/Program.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class Program {
+
+    //1. Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll.
+
+    //2. Создать MFU c функциями, сканирования, печати и ксерокопирования
+
+    public static void main(String[] args) {
+
+    }
+}
