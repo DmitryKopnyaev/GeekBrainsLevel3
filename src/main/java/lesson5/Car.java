f02503049a91b84045ac35f1b826ebea116ee4b0
Dimentor <dk2491@mail.ru>
2021-04-01 08:30:15 +0400

    Lesson 5: homework finished

--- src/main/java/lesson5/Car.java
+++ src/main/java/lesson5/Car.java
@@ -20,17 +20,25 @@ public class Car implements Runnable {
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            this.race.getWaitBeforeStart().await();
+            race.getStartRace().countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        if (!race.isWinnerFound()) {
+            race.setWinnerFound(true);
+            System.out.println(this.name + " - WIN");
+        }
+        race.getEndRace().countDown();
     }
 }
--- src/main/java/lesson5/Race.java
+++ src/main/java/lesson5/Race.java
@@ -1,11 +1,43 @@
 package lesson5;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class Race {
+    private final CyclicBarrier waitBeforeStart;
     private ArrayList<Stage> stages;
-    public ArrayList<Stage> getStages() { return stages; }
-    public Race(Stage... stages) {
+    private volatile boolean winnerFound;
+    private final CountDownLatch startRace;
+    private final CountDownLatch endRace;
+
+    public Race(int countPlayers, Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
+        this.waitBeforeStart = new CyclicBarrier(countPlayers);
+        winnerFound = false;
+        startRace = new CountDownLatch(countPlayers);
+        endRace = new CountDownLatch(countPlayers);
+    }
+
+    public boolean isWinnerFound() {
+        return winnerFound;
+    }
+
+    public synchronized void setWinnerFound(boolean winnerFound) {
+        this.winnerFound = winnerFound;
+    }
+
+    public CountDownLatch getStartRace() {
+        return startRace;
+    }
+
+    public CountDownLatch getEndRace() {
+        return endRace;
+    }
+
+    public ArrayList<Stage> getStages() { return stages; }
+
+    public CyclicBarrier getWaitBeforeStart() {
+        return waitBeforeStart;
     }
 }
